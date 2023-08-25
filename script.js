90dd70b0812c4e464be17878b92ebca50832912b
denchicka <denchicka@mail.ru>
2023-08-25 18:01:23 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ function navShow(x) {
     var linkContacts = document.getElementById("link__contacts");
     var linkKassy = document.getElementById("link__kassy");
     var navBar = document.getElementById("navBar__block");
-
     x.classList.toggle("change");
-
     if (navBar.style.height != "350px"){
         navBar.style.height = "350px";
-        linkMain.style.display = "block";
-        linkAbout.style.display = "block";
-        linkGallery.style.display = "block";
-        linkContacts.style.display = "block";
-        linkKassy.style.display = "block";
+        setTimeout(() => {
+                linkMain.style.display = "block";
+                linkAbout.style.display = "block";
+                linkGallery.style.display = "block";
+                linkContacts.style.display = "block";
+                linkKassy.style.display = "block";
+        }, 100)
     }
     else {
         navBar.style.height = "50px";
@@ -23,6 +23,5 @@ function navShow(x) {
         linkGallery.style.display = "none";
         linkContacts.style.display = "none";
         linkKassy.style.display = "none";
-    }
-    
+    }        
 }
